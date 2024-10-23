0f6bd31a9b3870dae7d3cef58c05a44e0da34ba8
shashank2497 <shasha.star724@gmail.com>
2024-10-23 23:50:43 +0530

    com

--- src/components/Github/Github.jsx
+++ src/components/Github/Github.jsx
@@ -3,15 +3,7 @@ import { useLoaderData } from 'react-router-dom'
 
 function Github() {
     const data = useLoaderData()
-    // const [data, setData] = useState([])
-    // useEffect(() => {
-    //  fetch('https://api.github.com/users/hiteshchoudhary')
-    //  .then(response => response.json())
-    //  .then(data => {
-    //     console.log(data);
-    //     setData(data)
-    //  })
-    // }, [])
+  
     
   return (
     <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github followers: {data.followers}
